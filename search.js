2d387a65ea279623a10c3632bbb0c3c3a62994c6
Yano <yanick.arn@sbb.ch>
2018-12-18 15:20:15 +0100

    Changes

--- search.js
+++ search.js
@@ -1,8 +1,8 @@
 String.prototype.replaceChars = function(character, replacement){
-    var str = this;
-    var a;
-    var b;
-    for(var i=0; i < str.length; i++){
+    let str = this;
+    let a;
+    let b;
+    for(let i=0; i < str.length; i++){
         if(str.charAt(i) == character){
             a = str.substr(0, i) + replacement;
             b = str.substr(i + 1);
@@ -13,31 +13,26 @@ String.prototype.replaceChars = function(character, replacement){
 }
  
 function search(query){
-    switch(query.substr(0, 2)){
-            
-        default:
-            window.location="https://www.google.com/search?client=firefox-b-ab&q=" +
-                query.replaceChars(" ", "+");
-    }
+    window.location="https://www.google.com/search?client=firefox-b-ab&q=" + query.replaceChars(" ", "+");
 }
  
 window.onload = function(){
     // search
-    searchinput = document.getElementById("searchbox");
+    let searchinput = document.getElementById("searchbox");
     if(!!searchinput){
         searchinput.addEventListener("keypress", function(a){
-            var key = a.keyCode;
+            let key = a.keyCode;
             if(key == 13){
-                var query = this.value;
+                let query = this.value;
                 search(query);
             }
         });
     }
  
     // jump to search when tab is pressed
-    var search_sqr = document.getElementById("search_sqr");
+    let search_sqr = document.getElementById("search_sqr");
  
-        }
+    }
  
 //
 // To add a new search provider, paste the following between the last "break;" and "default:" (Line 39 & 40)
